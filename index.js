c61844c6545de498f6a197a610f4f2c99c054d56
ChanchaiKunnasut <c.kunnasut@togetherteam.co.th>
2017-12-14 16:26:59 +0700

    update writehead2

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {
         "Content-Type": "text/plain; charset=utf-8",
-        "CF-RAY": "3cd014c7c90b17b6-SIN"
+        "CF-RAY": "3cd014c7c90b17b6-SIN",
+        "Sponsored-By": "https://www.runscope.com",
+        "Strict-Transport-Security": "max-age=15552000",
+        "Transfer-Encoding": "chunked",
+        "Via": "1.1 vegur",
+        "X-Content-Type-Options": "nosniff"
+
+
+
     });
     response.end("ok");
 
