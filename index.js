1dc37a632b7fdf9253346128382347c5d8d7c9aa
ChanchaiKunnasut <c.kunnasut@togetherteam.co.th>
2017-12-15 10:56:48 +0700

    create new web for test SignalR

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-var http = require('http');
+const express = require("express");
+const app = express();
+const path = require("path");
 
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {
-        "Content-Type": "text/plain; charset=utf-8",
-        "CF-RAY": "3cd014c7c90b17b6-SIN",
-        "Sponsored-By": "https://www.runscope.com",
-        "Strict-Transport-Security": "max-age=15552000",
-        "Transfer-Encoding": "chunked",
-        "Via": "1.1 vegur",
-        "X-Content-Type-Options": "nosniff",
-        "Connection": "keep-alive"
-    });
-    response.end("ok");
-
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-var port = process.env.PORT || 1337;
-server.listen(port);
+const port = process.env.PORT || 1337;
+app.listen(port,() => console.log("Server running at http://locahost:%d",port));
 
-console.log("Server running at http://localhost:%d", port);
