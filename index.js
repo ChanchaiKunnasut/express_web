11317b562a7d70cee743efa867b185e66bb9e3cc
ChanchaiKunnasut <c.kunnasut@togetherteam.co.th>
2017-12-18 14:05:23 +0700

    #5

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+app.get('/Scripts',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/jquery-1.10.2.min.js'));
+});
+
+app.get('/Scripts',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/jquery.signalR-2.2.2.min.js'));
+});
+
 const port = process.env.PORT || 1337;
 app.listen(port,() => console.log("Server running at http://locahost:%d",port));
 
