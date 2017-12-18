fb7a5d0b333fc03a2be603c39fdc3ea8fd389a99
ChanchaiKunnasut <c.kunnasut@togetherteam.co.th>
2017-12-18 14:03:18 +0700

    #4

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// const express = require("express");
-// const app = express();
-// const path = require("path");
+const express = require("express");
+const app = express();
+const path = require("path");
 
-// app.get('/',(req,res)=>{
-//     res.sendFile(path.join(__dirname+'/index.html'));
-// });
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
 
-// const port = process.env.PORT || 1337;
-// app.listen(port,() => console.log("Server running at http://locahost:%d",port));
+const port = process.env.PORT || 1337;
+app.listen(port,() => console.log("Server running at http://locahost:%d",port));
 
